3833ca1fee736ed0ba5a6b74bf9a592158f0a8dd
DESKTOP-I2CCB16\suton <naoki_matsuura@earlycross.co.uk>
2024-08-02 14:45:04 +0900

    web page

--- src/TopImages.tsx
+++ src/TopImages.tsx
@@ -1,10 +1,10 @@
 
 const topImages = [
-  "/img/top/001.png",
-  "/img/top/002.png",
-  "/img/top/003.png",
-  "/img/top/004.png",
-  "/img/top/005.png",
+  "img/top/001.png",
+  "img/top/002.png",
+  "img/top/003.png",
+  "img/top/004.png",
+  "img/top/005.png",
 ];
 
 export default topImages;
